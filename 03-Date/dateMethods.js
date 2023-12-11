bd1fa7d72a6760d0732f70345730ec365bc66e4a
vaishali86c <vaishali86c@gmail.com>
2023-12-11 19:58:29 +0530

    Upload date and math methods file

---
+++ 03-Date/dateMethods.js
@@ -0,0 +1,6 @@
+const date = new Date();
+
+console.log(date);
+
+// current time 
+
---
+++ 06-MathAndNumber/number.js
@@ -0,0 +1,11 @@
+const scores = 20;
+console.log(scores);
+
+const num = new Number(100);
+console.log(num);
+console.log(num.toString().length);
+console.log(typeof(num));
+console.log(num.toFixed(2));
+
+// method : toPrecision
+// toLocalString
