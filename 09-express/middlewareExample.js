ddbdb068c64c261eb2b40cec4d2cfc74b1c4163c
vaishali86c <vaishali86c@gmail.com>
2023-12-29 16:44:48 +0530

    Add middlewareEaxample file

--- 09-express/middlewareExample.js
+++ 09-express/middlewareExample.js
@@ -4,24 +4,30 @@ const app = express();
 
 // function that return a booelan if the age of the person is more than 14
 
-function isOldEnough(age) {
+function isOldEnoughMiddleware(age) {
+    const age = req.query.age;
+
     if (age >= 14) {
-        return true;
+        next();
     } else {
-        return false;
+        res.json({
+            msg : "Sorry you are not in age yet"
+        })
     }
 }
 
-app.get("/ride1", function(req, res) {
-    if (isOldEnough(req.query.age)) {
-        res.json({
-            msgs: "You have successfully riden the ride 1"
-        })
-    } else {
-        res.status(401).json({
-            msgs: "sorry you are not in the age"
-        })
-    }
+app.get("/ride1", isOldEnoughMiddleware, function(req, res) {
+
+    res.json({
+        msgs: "You have successfully riden the ride 1"
+    })
+})
+
+app.get("/ride2", isOldEnoughMiddleware, function(req, res) {
+    
+    res.json({
+        msgs: "You have successfully riden the ride 2"
+    })
 })
 
 app.listen(3000);
