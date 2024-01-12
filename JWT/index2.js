bd33859de31828fc12cf131577eaf75b4d3b3885
vaishali86c <vaishali86c@gmail.com>
2024-01-12 20:21:13 +0530

    Added file in Jwt folder

---
+++ JWT/index2.js
@@ -0,0 +1,13 @@
+const welcome = 'welcome to 30 days of react';
+const title = 'Getting started react';
+const subtitle = 'javscript Library';
+const authorFirstname = 'ADA';
+const date = 'Oct 1, 2022';
+
+// JSX element , header
+
+const header = (
+    <header>
+        <div>{welcome}</div>         
+    </header>
+)
