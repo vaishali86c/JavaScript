515822f231c25d13e297ccf772519ba9d8f6a887
vaishali86c <vaishali86c@gmail.com>
2024-01-14 23:34:38 +0530

    Solved middleware errorcount assignment

--- assignment/week-3/01-middlewares/03-errorcount.js
+++ assignment/week-3/01-middlewares/03-errorcount.js
@@ -1,37 +1,73 @@
+// const express = require('express');
+
+// const app = express();
+// let errorCount = 0;
+
+// // You have been given an express server which has a few endpoints.
+// // Your task is to
+// // 1. Ensure that if there is ever an exception, the end user sees a status code of 404
+// // 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
+
+// app.get('/user', function (req, res) {
+
+//   throw new Error("some error");
+
+//   res.status(200).json({name: 'john'});
+// })
+
+// app.post('/user', function (req, res) {
+//   res.status(200).json({msg: 'created dummy user'});
+// });
+
+// app.get('errorCount', function (req, res) {
+//   res.status(200).json(errorCount);
+// });
+
+// // error handling middleware
+
+// app.use(function (err, req, res, next) {
+//   res.status(404).send({})
+//   errorCount = errorCount + 1;
+// })
+
+// app.listen(3000);
+
+// // Error-handling middleware
+
+
 const express = require('express');
 
 const app = express();
 let errorCount = 0;
 
-// You have been given an express server which has a few endpoints.
-// Your task is to
-// 1. Ensure that if there is ever an exception, the end user sees a status code of 404
-// 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
-
-// Error-handling middleware
-app.use(function(err, req, res, next) {
-  errorCount += 1;
-  res.status(404).json({ error: 'Not Found' });
+app.get('/user', function (req, res) {
+  try {
+    throw new Error("some error");
+    // The following code will not be executed if an error is thrown
+    res.status(200).json({ name: 'john' });
+  } catch (error) {
+    // Handle the error here and send an appropriate response
+    res.status(404).json({ error: error.message });
+    errorCount += 1;
+  }
 });
 
-app.get('/user', function(req, res) {
-  // Simulate an error by throwing an exception
-  throw new Error("User not found");
-});
-
-app.post('/user', function(req, res) {
+app.post('/user', function (req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
 
-app.get('/errorCount', function(req, res) {
+app.get('/errorCount', function (req, res) {
   res.status(200).json({ errorCount });
 });
 
-// Start the server
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Error-handling middleware
+app.use(function (err, req, res, next) {
+  res.status(404).json({});
+  errorCount += 1;
 });
+
+app.listen(3000);
+
 
 
 module.exports = app;
